9b10b2bcdcc0e57137e00939f7a9878d4e7ac25d
Isabella Beltrame <isabeltrameg12@gmail.com>
2021-11-26 22:02:39 -0300

    upload

--- beeive.js
+++ beeive.js
@@ -1,5 +1,4 @@
-
-const sendEmailElement = document.getElementById('send_email');
+/*const sendEmailElement = document.getElementById('send_email');
 sendEmailElement.addEventListener('click', () => {
-   window.location.href = 'mailto:isabeltrameg12@gmail.com';
-});
+    window.location.href = 'mailto:isabeltrameg12@gmail.com';
+});*/
